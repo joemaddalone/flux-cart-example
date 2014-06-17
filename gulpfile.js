afe5037d52c5d5fc9f09f8d4c364729cb07ea2de
Joe Maddalone <joe@insert-title.com>
2014-06-16 22:10:03 -0500

    moves main.js to dist/js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('browserify', function() {
     gulp.src(['src/js/main.js'])
     .pipe(browserify({transform: 'reactify'}))
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/js'))
 });
 
 gulp.task('copy',function() {
