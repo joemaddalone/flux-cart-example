7d8e41bda4e0d69218e163823e200f394dc4b7d7
Joe Maddalone <joe@insert-title.com>
2014-10-08 09:21:50 -0500

    removed es6-promise since it's now a global in node

--- src/js/dispatcher/dispatcher.js
+++ src/js/dispatcher/dispatcher.js
@@ -1,6 +1,5 @@
 'use strict';
-var Promise = require('es6-promise').Promise,
-    merge = require('react/lib/merge');
+var merge = require('react/lib/merge');
 
 var _callbacks = [];
 var _promises = [];
